7269040e9de3c68c85c946ec2e38435c8becb0b9
Amit <amitc9der@gmail.com>
2025-03-23 00:30:49 +0530

    Added Tabs Functionallty press Alt + 1 & Alt + 2 to switch.

--- Readme.md
+++ Readme.md
@@ -1,2 +1,4 @@
+## Running Example
+[Live](https://amitc9der.github.io/web_projects)
 ### Displaying Text in Canvas
 ![Text Metrics](./docs/images/TextMatricsNotes.png)
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+
 //Display The Canvas's dimensions.
+
 // /** @typedef {typeof Promise} PromiseConstructor */
 /** 
  * @param { CanvasRenderingContext2D } ctx - context
@@ -16,46 +18,64 @@ async function displayCanvasDimension(canvas, ctx){
 }
 
 //Display Text
+/**
+ * @typedef { Object } DisplayTextType
+ * @property { string } txt
+ * @property { number } x
+ * @property { number } y 
+ * @property { CanvasRenderingContext2D } ctx  
+ * @property { string } font 
+ * @property { number } shadowOffsetX - shadowOffsetX 
+ * @property { number } shadowOffsetY - showdowOffsetY 
+ */
+
 /** 
- * @param { string } txt - context 
- * @param { number } x - context 
- * @param { number } y - context 
- * @param { CanvasRenderingContext2D } ctx - context 
- * @param { font } txt - context 
- * @param { number } shadowOffsetX - context 
- * @param { number } shadowOffsetY - context 
- * @returns {function(ctx): void}
+ * @param { string } txt - text to display 
+ * @param { number } x - x corrdination 
+ * @param { number } y - y  corrdination 
+ * @param { CanvasRenderingContext2D } ctx - canvas context 
+ * @param { string } font - font for text 
+ * @param { string } fillStyle - font for text 
+ * @param { number } shadowOffsetX - shadowOffsetX 
+ * @param { number } shadowOffsetY - shadowOffsetY 
+ * @returns { DisplayTextType }
 */
 //FIXME: Clear The Preivous Text;
-function DisplayText(txt,x,y,ctx,font = "20px Times New Roman", shadowOffsetX = 0,shadowOffsetY = 0){
+function DisplayText(txt,x,y,ctx,font = "20px Times New Roman", fillStyle = "Black" ,shadowOffsetX = 0,shadowOffsetY = 0){
   let _shadowOffsetX = shadowOffsetX ? shadowOffsetX : 0;
   let _shadowOffsetY =  shadowOffsetY ? shadowOffsetY : 0;
-  let textMetrics = ctx.measureText(txt);
-  const boxStartX = x - textMetrics.actualBoundingBoxLeft;
-  const boxStartY = y - textMetrics.actualBoundingBoxAscent; 
-
-  /** @type { function(ctx): void} */
-  let clearCallback;
-
-  /** @type { function(ctx): void} */
-  clearCallback = (c)=>{
-    const textHeight = textMetrics.fontBoundingBoxAscent + textMetrics.fontBoundingBoxDescent + _shadowOffsetY;
-    const textWidth = textMetrics.actualBoundingBoxLeft + textMetrics.actualBoundingBoxRight + _shadowOffsetX;
-    // c.clearRect(x-textMetrics.actualBoundingBoxLeft ,y-textMetrics.fontBoundingBoxAscent +_shadowOffsetY,x+textWidth,textHeight);
-    c.clearRect(boxStartX  - _shadowOffsetX, boxStartY - _shadowOffsetY, textWidth,textHeight);
-    // c.fillStyle = "rgb(200 0 0)";
-    // c.strokeRect(x-textMetrics.actualBoundingBoxLeft,y-textMetrics.fontBoundingBoxAscent,textWidth,textHeight);
-  }
-  clearCallback(ctx);
 
   ctx.shadowOffsetX = _shadowOffsetX;
   ctx.shadowOffsetY = _shadowOffsetY;
-  ctx.fillStyle = "Black";
-  ctx.font = "20px Times New Roman";
+  ctx.fillStyle = fillStyle;
+  ctx.font = font;
   ctx.fillText(txt,x,y);
+  const displayText = {
+    txt:txt,
+    ctx:ctx,
+    x:x,
+    y:y,
+    shadowOffsetX:_shadowOffsetX,
+    shadowOffsetY:_shadowOffsetY,
+    font:font
+  };
 
-  return clearCallback;
+  return displayText;
 
+}
+
+/** @param { DisplayTextType } t -- DisplayTextType */
+const ClearText = (t)=>{
+  if(t.txt ==="" || t.ctx === undefined || t.x === undefined || t.y === undefined){
+    return
+  }
+  const textMetrics = t.ctx.measureText(t.txt);
+  console.log("Text Metrics:- ",textMetrics);
+  const boxStartX = t.x;
+  const boxStartY = t.y - textMetrics.actualBoundingBoxAscent; 
+  const textHeight = textMetrics.fontBoundingBoxAscent + textMetrics.fontBoundingBoxDescent;
+  const textWidth = Math.round(textMetrics.actualBoundingBoxLeft + textMetrics.actualBoundingBoxRight);
+  t.ctx.clearRect(boxStartX , boxStartY, textWidth + t.shadowOffsetX,textHeight + t.shadowOffsetY);
 }
 
 //Display mouse position.
@@ -63,47 +83,59 @@ function DisplayText(txt,x,y,ctx,font = "20px Times New Roman", shadowOffsetX = 
  *@param { HTMLCanvasElement } canvas  - Canvas
  * @param { CanvasRenderingContext2D } ctx - context
 */
-//FIXME: //Need to fix the drawring over probelm. 
 async function DisplayCursorPosition(canvas,ctx){
   const x = 10;
   const y = 70;
-  /** @type { function(ctx): void} */
-  let clearCallback;
+
+  /** @type { DisplayTextType } */
+  let prevText = {
+    txt: "",
+    shadowOffsetX: 0,
+    shadowOffsetY: 0,
+    x: 0,
+    y: 0,
+    ctx: undefined,
+    font: ""
+  };
 
   canvas.addEventListener("mousemove",(event)=>{
+    ClearText(prevText);
     const text = `Mouse is at X: ${event.clientX}, Y: ${event.clientY}`;
-    clearCallback = DisplayText(text,x,y,ctx);
+    prevText = DisplayText(text,x,y,ctx);
   })
   canvas.addEventListener("mouseover",(event)=>{
     // console.log("Mouse Over",event);
   })
   canvas.addEventListener("mouseleave",(event)=>{
-    clearCallback(ctx);
+    ClearText(prevText);
   })
 }
+
 //Capture all keyboard input except keyboard reserved by browser and windows.
 //Capture Mouse Input
 
 /**
- * @param { HTMLCanvasElement } canvas - Canvas
  * @returns { void }
 */
-function canvas_render(canvas) {
-  // console.log("Rerendering canvas");
-
-  /** @type {CanvasRenderingContext2D } */
-  const ctx = canvas.getContext("2d");
-  // console.log(ctx);
-
+function canvas_render() {
+  console.log("Rerendering canvas");
   if (ctx) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    displayCanvasDimension(canvas,ctx);
-    DisplayCursorPosition(canvas,ctx);
-    // ctx.fillStyle = "rgb(200 0 0)";
-    // ctx.fillRect(100, 100, 50, 50);
-
-    // ctx.fillStyle = "rgb(0 0 200 / 50%)";
-    // ctx.fillRect(130, 130, 50, 50);
+    console.log(active_tab);
+    switch (active_tab) {
+      case 1:
+        displayCanvasDimension(canvas,ctx);
+        DisplayCursorPosition(canvas,ctx);
+      case 2:
+        ctx.fillStyle = "rgb(200 0 0)";
+        ctx.fillRect(100, 100, 50, 50);
+        ctx.fillStyle = "rgb(0 0 200 / 50%)";
+        ctx.fillRect(130, 130, 50, 50);
+        break;
+      default:
+        break;
+    }
+    requestAnimationFrame(canvas_render);
 
   } else {
     console.log("Unable to get context");
@@ -115,28 +147,55 @@ function canvas_render(canvas) {
  * @returns { void }
 */
 function body_mutation_callback(entries) {
-  // console.log(entries);
   const canvas = document.getElementsByTagName("canvas")[0];
   canvas.height = window.innerHeight;
   canvas.width = entries[0].contentBoxSize[0].inlineSize;
-  console.log(canvas.height,canvas.width);
-  canvas_render(canvas);
+  canvas_render();
 }
 
 function main() {
-  // console.log("JS linked");
+  console.log("JS linked");
 
   /**@type { HTMLBodyElement } */
   const body_el = document.getElementsByTagName("body")[0];
   body_el.style.margin = "0px";
 
-  /**@type { HTMLCanvasElement } */
-  const canvas = document.createElement("canvas");
+  canvas = document.createElement("canvas");
   body_el.appendChild(canvas);
+  ctx = canvas.getContext("2d")
 
   /** @type { ResizeObserver } */
   const body_resize_observer = new ResizeObserver(body_mutation_callback);
   body_resize_observer.observe(document.documentElement);
+
+  window.requestAnimationFrame(canvas_render)
+
+  window.addEventListener("keydown",(e)=>{
+    if(e.altKey){
+      if(caputre_keyboard){
+        e.preventDefault();
+      }
+      console.log(e);
+      switch (e.code) {
+        case 'Digit1':
+          active_tab = 1;
+          break;
+        case 'Digit2':
+          active_tab = 2;
+          break;
+        default:
+          break;
+      }
+    }
+  });
 }
 
+var active_tab = 1;
+var caputre_keyboard = 1;
+
+/** @type {HTMLCanvasElement | null} -- canvas*/
+var canvas ;
+
+/** @type {CanvasRenderingContext2D } */
+var ctx ;
 document.body.onload = main;
